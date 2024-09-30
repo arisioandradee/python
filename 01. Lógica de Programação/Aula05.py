c66b2ebfa308ce6aaf38ede0a8cc200de7a83489
arisioandradee <arisiosaf@gmail.com>
2024-09-30 20:41:53 -0300

    Adicionando Arquivos

---
+++ 01. Lógica de Programação/Aula05.py
@@ -0,0 +1,20 @@
+#Concatenação(+) e repetição (*) com operadores + Predecência entre operadores
+contatenacao = 'Arisio' + ' ' + 'Andrade'
+print(contatenacao)
+
+a_dez_vezes = 'A' * 10
+arisio_tres_vezes = 'Arisio ' * 3
+
+print(a_dez_vezes)
+print(arisio_tres_vezes)
+
+# Predecência entre operadores(ordem que o programa realiza:)
+# 1. (n + n)
+# 2. **
+# 3. * / // %
+# 4. + -
+
+conta_1 = (1+1) ** 5+5
+print(conta_1)
+
+
