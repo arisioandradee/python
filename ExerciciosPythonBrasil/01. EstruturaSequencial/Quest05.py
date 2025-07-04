9f9cffba5f43d29f44c1a787e1b5a34088711571
Arisio <arisiosaf@gmail.com>
2025-07-04 10:37:54 -0300

    Removing old files

--- ExerciciosPythonBrasil/01. EstruturaSequencial/Quest05.py
+++
@@ -1,8 +0,0 @@
-# Faça um Programa que converta metros para centímetros.
-
-metros = input('Digite um valor em metros: ')
-metros = float(metros)
-
-cen = metros * 100
-
-print(f'O valor convertido para centimetros é: {cen}')
--- ExerciciosPythonBrasil/01. EstruturaSequencial/Quest10.py
+++
@@ -1,7 +0,0 @@
-# Faça um Programa que peça a temperatura em graus Celsius, transforme e mostre em graus Fahrenheit.
-
-cel = input('Digite o grau em Celcius: ')
-cel = float(cel)
-fah = (cel * (9/5)) + 32
-
-print(f'A quantidade de graus após converter é: {fah:.2f}°F')
